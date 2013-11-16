03bbdf71455b50d09b3fc1cce6c7135264660847
duncanbrown <duncanphilipbrown@hotmail.com>
2013-11-16 12:00:15 +0000

    hw-controller

---
+++ AngularTesting/Scripts/app/controllers/hw-controller.js
@@ -0,0 +1,11 @@
+﻿function HelloWorldController($scope) {
+    $scope.name = 'World';
+
+    Object.defineProperty($scope, 'nameChars', {
+        get: function () { return $scope.name.length; }
+    });
+
+    //$scope.nameChars = function () {
+    //    return $scope.name.length;
+    //};
+}
